9bbcc70b2c8239ef88050bc40243bcaed54f4d5c
Boquan (Brian) Yin <57741529+Toubat@users.noreply.github.com>
2022-03-13 16:32:14 -0500

    upload assignment

--- assignment3_part1/classifier.py
+++ assignment3_part1/classifier.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
-from torch import optim
+from torch import Tensor, optim
 import numpy as np
 
 NUM_CLASSES = 21
@@ -30,11 +30,246 @@ class SimpleClassifier(nn.Module):
         return x
 
 
-class Classifier(nn.Module):
-    # TODO: implement me
-    def __init__(self):
-        super(Classifier, self).__init__()
-        return
+class ConvBlock(nn.Module):
 
-    def forward(self, x):
-        return
+    def __init__(self, in_channels, out_channels, num_layers: int, bn: bool, skip_connect: bool):
+        super().__init__()
+
+        self.num_layers = num_layers
+        self.skip_connect = skip_connect
+        self.bn = bn
+
+        self.downsample = nn.Sequential(
+          nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+          nn.BatchNorm2d(out_channels)
+        )
+
+        self.convs = nn.ModuleList([nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1) for _ in range(num_layers-1)])
+        self.bns = nn.ModuleList([nn.BatchNorm2d(out_channels) for _ in range(num_layers-1)])
+
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.downsample(x)
+        identity = x
+
+        for i in range(self.num_layers-1):
+            x = self.convs[i](x)
+
+            if self.bn: # BatchNorm
+                x = self.bns[i](x)
+
+            x = self.relu(x)
+
+
+        if self.skip_connect:
+            x += identity
+
+        x = self.maxpool(x)
+        return x
+
+
+class ConvNet(nn.Module):
+
+    def __init__(self, num_layers, bn=False, skip_connect=False, num_classes=21):
+        super().__init__()
+
+        self.in_channels = 3
+
+        self.layer1 = ConvBlock(self.in_channels, 64, num_layers[0], bn, skip_connect)
+        self.layer2 = ConvBlock(64, 128, num_layers[1], bn, skip_connect)
+        self.layer3 = ConvBlock(128, 256, num_layers[2], bn, skip_connect)
+        self.layer4 = ConvBlock(256, 512, num_layers[3], bn, skip_connect)
+        self.layer5 = ConvBlock(512, 512, num_layers[4], bn, skip_connect)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((5, 5))
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 5 * 5, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes)
+        )
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+
+        return x
+
+
+class BasicBlock(nn.Module):
+
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, downsample=None, stride=1):
+        super().__init__()
+
+        self.downsample = downsample
+
+        # Conv 3 x 3
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        # Conv 3 x 3
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        x += identity
+        x = F.relu(x)
+
+        return x
+
+
+class BottleNeck(nn.Module):
+
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, downsample=None, stride=1):
+        super().__init__()
+
+        self.downsample = downsample
+
+        # Conv 1 x 1
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        # Conv 3 x 3
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        # Conv 1 x 1 with expansion
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        x += identity
+        x = F.relu(x)
+
+        return x
+
+
+class DropoutBlock(nn.Module):
+
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, downsample=None, stride=1):
+        super().__init__()
+
+        self.downsample = downsample
+
+        # Conv 1 x 1
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        # Conv 3 x 3
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        # Conv 1 x 1 with expansion
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+
+        self.dropout = nn.Dropout(p=0.7)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        x = F.relu(self.bn1(self.dropout(self.conv1(x))))
+        x = F.relu(self.bn2(self.dropout(self.conv2(x))))
+        x = self.bn3(self.conv3(x))
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        x += identity
+        x = F.relu(x)
+
+        return x
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, num_layers, input_channels, num_classes):
+        super().__init__()
+
+        self.in_channels = 64
+        self.block = block
+
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.make_layer(num_layers[0], out_channels=64, stride=1)
+        self.layer2 = self.make_layer(num_layers[1], out_channels=128, stride=2)
+        self.layer3 = self.make_layer(num_layers[2], out_channels=256, stride=2)
+        self.layer4 = self.make_layer(num_layers[3], out_channels=512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = F.relu(self.bn1(self.conv1(x)))
+
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+    def make_layer(self, num_blocks, out_channels, stride):
+        downsample = None
+        layers = []
+
+        if stride != 1 or self.in_channels != out_channels * self.block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * self.block.expansion, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels * self.block.expansion)
+            )
+
+        layers.append(self.block(self.in_channels, out_channels, downsample, stride))
+        self.in_channels = out_channels * self.block.expansion
+
+        for i in range(1, num_blocks):
+            layers.append(self.block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
---
+++ assignment3_part2/yolo_loss.py
@@ -0,0 +1,200 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+def compute_iou(box1, box2):
+    """Compute the intersection over union of two set of boxes, each box is [x1,y1,x2,y2].
+    Args:
+      box1: (tensor) bounding boxes, sized [N,4].
+      box2: (tensor) bounding boxes, sized [M,4].
+    Return:
+      (tensor) iou, sized [N,M].
+    """
+    N = box1.size(0)
+    M = box2.size(0)
+
+    lt = torch.max(
+        box1[:, :2].unsqueeze(1).expand(N, M, 2),  # [N,2] -> [N,1,2] -> [N,M,2]
+        box2[:, :2].unsqueeze(0).expand(N, M, 2),  # [M,2] -> [1,M,2] -> [N,M,2]
+    )
+
+    rb = torch.min(
+        box1[:, 2:].unsqueeze(1).expand(N, M, 2),  # [N,2] -> [N,1,2] -> [N,M,2]
+        box2[:, 2:].unsqueeze(0).expand(N, M, 2),  # [M,2] -> [1,M,2] -> [N,M,2]
+    )
+
+    wh = rb - lt  # [N,M,2]
+    wh[wh < 0] = 0  # clip at 0
+    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+
+    area1 = (box1[:, 2] - box1[:, 0]) * (box1[:, 3] - box1[:, 1])  # [N,]
+    area2 = (box2[:, 2] - box2[:, 0]) * (box2[:, 3] - box2[:, 1])  # [M,]
+    area1 = area1.unsqueeze(1).expand_as(inter)  # [N,] -> [N,1] -> [N,M]
+    area2 = area2.unsqueeze(0).expand_as(inter)  # [M,] -> [1,M] -> [N,M]
+
+    iou = inter / (area1 + area2 - inter)
+    return iou
+
+
+class YoloLoss(nn.Module):
+    def __init__(self, S, B, l_coord, l_noobj):
+        super(YoloLoss, self).__init__()
+        self.S = S
+        self.B = B
+        self.l_coord = l_coord
+        self.l_noobj = l_noobj
+
+    def xywh2xyxy(self, boxes):
+        """
+        Parameters:
+        boxes: (N,4) representing by x,y,w,h
+
+        Returns:
+        boxes: (N,4) representing by x1,y1,x2,y2
+
+        if for a Box b the coordinates are represented by [x, y, w, h] then
+        x1, y1 = x/S - 0.5*w, y/S - 0.5*h ; x2,y2 = x/S + 0.5*w, y/S + 0.5*h
+        Note: Over here initially x, y are the center of the box and w,h are width and height.
+        """
+        ### CODE ###
+        # Your code here
+
+        return boxes
+
+    def find_best_iou_boxes(self, pred_box_list, box_target):
+        """
+        Parameters:
+        box_pred_list : [(tensor) size (-1, 4) ...]
+        box_target : (tensor)  size (-1, 5)
+
+        Returns:
+        best_iou: (tensor) size (-1, 1)
+        best_boxes : (tensor) size (-1, 5), containing the boxes which give the best iou among the two (self.B) predictions
+
+        Hints:
+        1) Find the iou's of each of the 2 bounding boxes of each grid cell of each image.
+        2) For finding iou's use the compute_iou function
+        3) use xywh2xyxy to convert bbox format if necessary,
+        Note: Over here initially x, y are the center of the box and w,h are width and height.
+        We perform this transformation to convert the correct coordinates into bounding box coordinates.
+        """
+
+        ### CODE ###
+        # Your code here
+        return best_ious, best_boxes
+
+    def get_class_prediction_loss(self, classes_pred, classes_target, has_object_map):
+        """
+        Parameters:
+        classes_pred : (tensor) size (batch_size, S, S, 20)
+        classes_target : (tensor) size (batch_size, S, S, 20)
+        has_object_map: (tensor) size (batch_size, S, S)
+
+        Returns:
+        class_loss : scalar
+        """
+        ### CODE ###
+        # Your code here
+        return loss
+
+    def get_no_object_loss(self, pred_boxes_list, has_object_map):
+        """
+        Parameters:
+        pred_boxes_list: (list) [(tensor) size (N, S, S, 5)  for B pred_boxes]
+        has_object_map: (tensor) size (N, S, S)
+
+        Returns:
+        loss : scalar
+
+        Hints:
+        1) Only compute loss for cell which doesn't contain object
+        2) compute loss for all predictions in the pred_boxes_list list
+        3) You can assume the ground truth confidence of non-object cells is 0
+        """
+        ### CODE ###
+        # Your code here
+
+        return loss
+
+    def get_contain_conf_loss(self, box_pred_conf, box_target_conf):
+        """
+        Parameters:
+        box_pred_conf : (tensor) size (-1,1)
+        box_target_conf: (tensor) size (-1,1)
+
+        Returns:
+        contain_loss : scalar
+
+        Hints:
+        The box_target_conf should be treated as ground truth, i.e., no gradient
+
+        """
+        ### CODE
+        # your code here
+        return loss
+
+    def get_regression_loss(self, box_pred_response, box_target_response):
+        """
+        Parameters:
+        box_pred_response : (tensor) size (-1, 4)
+        box_target_response : (tensor) size (-1, 4)
+        Note : -1 corresponds to ravels the tensor into the dimension specified
+        See : https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view_as
+
+        Returns:
+        reg_loss : scalar
+
+        """
+        ### CODE
+        # your code here
+        return reg_loss
+
+    def forward(self, pred_tensor, target_boxes, target_cls, has_object_map):
+        """
+        pred_tensor: (tensor) size(N,S,S,Bx5+20=30) N:batch_size
+                      where B - number of bounding boxes this grid cell is a part of = 2
+                            5 - number of bounding box values corresponding to [x, y, w, h, c]
+                                where x - x_coord, y - y_coord, w - width, h - height, c - confidence of having an object
+                            20 - number of classes
+
+        target_boxes: (tensor) size (N, S, S, 4): the ground truth bounding boxes
+        target_cls: (tensor) size (N, S, S, 20): the ground truth class
+        has_object_map: (tensor, bool) size (N, S, S): the ground truth for whether each cell contains an object (True/False)
+
+        Returns:
+        loss_dict (dict): with key value stored for total_loss, reg_loss, containing_obj_loss, no_obj_loss and cls_loss
+        """
+        N = pred_tensor.size(0)
+        total_loss = 0.0
+
+        # split the pred tensor from an entity to separate tensors:
+        # -- pred_boxes_list: a list containing all bbox prediction (list) [(tensor) size (N, S, S, 5)  for B pred_boxes]
+        # -- pred_cls (containing all classification prediction)
+
+        # compcute classification loss
+
+        # compute no-object loss
+
+        # Re-shape boxes in pred_boxes_list and target_boxes to meet the following desires
+        # 1) only keep having-object cells
+        # 2) vectorize all dimensions except for the last one for faster computation
+
+        # find the best boxes among the 2 (or self.B) predicted boxes and the corresponding iou
+
+        # compute regression loss between the found best bbox and GT bbox for all the cell containing objects
+
+        # compute contain_object_loss
+
+        # compute final loss
+
+        # construct return loss_dict
+        loss_dict = dict(
+            total_loss=...,
+            reg_loss=...,
+            containing_obj_loss=...,
+            no_obj_loss=...,
+            cls_loss=...,
+        )
+        return loss_dict
